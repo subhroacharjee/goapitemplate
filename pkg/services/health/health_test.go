a74e755531fb4206c65c93d8da230b6aeb54f777
subhroacharjee <sacharjeeofficial@gmail.com>
2024-02-03 23:50:49 +0530

    fix: removing implementation details

--- pkg/services/health/health_test.go
+++ pkg/services/health/health_test.go
@@ -5,24 +5,38 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/subhroacharjee/appname/config"
+	"github.com/subhroacharjee/appname/utils/db"
 	testutils "github.com/subhroacharjee/appname/utils/test_utils"
 	"go.uber.org/zap"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
+
+type mockConfig struct{}
+
+// GetDSN implements config.Config.
+func (mockConfig) GetDSN() string {
+	panic("unimplemented")
+}
+
+// GetEnv implements config.Config.
+func (mockConfig) GetEnv() config.Env {
+	return config.Test
+}
+
+// GetPort implements config.Config.
+func (mockConfig) GetPort() uint {
+	panic("unimplemented")
+}
 
 func TestHealthService(t *testing.T) {
 	ctx := context.Background()
+	config := mockConfig{}
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	db, err := db.NewDb(config)
 	defer testutils.FindTestDbAndRemove("test.db")
 
 	healthService := HealthService{
